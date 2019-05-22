7d8748480192dba91a2e85675b1f8cfdc1c05309
noudadrichem <info@noudadrichem.com>
2019-05-22 10:11:18 +0200

    dpp4 hijberneet

---
+++ src/hijberneet/Main.java
@@ -0,0 +1,41 @@
+package hijberneet;
+
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import org.hibernate.Transaction;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Main {
+	public static void main(String[] args) throws SQLException, ParseException {
+		
+		System.out.println("hoi");
+
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+//		Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+//		SessionFactory factory = meta.getSessionFactoryBuilder().build();
+//		
+//		
+//		Session session = factory.openSession();
+//		Transaction t = session.beginTransaction();
+//
+//		Reiziger r = new Reiziger();
+//		r.setReizigerId(10);
+//		r.setAchternaam("Dijkstra");
+//		r.setGeboortedatum(new SimpleDateFormat("dd-mm-yy").parse("06-12-80"));
+//		r.setVoorletters("JW");
+//
+//		session.save(r);
+//
+//		t.commit();
+//		System.out.println("successfully saved");
+//		factory.close();
+//		session.close();
+	}
+}
