58faa57ee954246ed7bd68b24f1af7cf721b2cda
info@noudadrichem.com <info@noudadrichem.com>
2019-05-27 15:22:35 +0200

    hibernate done

--- src/hijberneet/Main.java
+++ src/hijberneet/Main.java
@@ -1,6 +1,5 @@
 package hijberneet;
 
-import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -13,29 +12,35 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class Main {
-	public static void main(String[] args) throws SQLException, ParseException {
+	public static void main(String[] args) {
 		
 		System.out.println("hoi");
 
 		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
-//		Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
-//		SessionFactory factory = meta.getSessionFactoryBuilder().build();
-//		
-//		
-//		Session session = factory.openSession();
-//		Transaction t = session.beginTransaction();
-//
-//		Reiziger r = new Reiziger();
-//		r.setReizigerId(10);
-//		r.setAchternaam("Dijkstra");
-//		r.setGeboortedatum(new SimpleDateFormat("dd-mm-yy").parse("06-12-80"));
-//		r.setVoorletters("JW");
-//
-//		session.save(r);
-//
-//		t.commit();
-//		System.out.println("successfully saved");
-//		factory.close();
-//		session.close();
+		System.out.print(ssr);
+		Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+		SessionFactory factory = meta.getSessionFactoryBuilder().build();
+		
+		
+		Session session = factory.openSession();
+		Transaction t = session.beginTransaction();
+
+		Reiziger r = new Reiziger();
+		r.setReizigerId(10);
+		r.setAchternaam("Dijkstra");
+		try {
+			r.setGeboortedatum(new SimpleDateFormat("dd-mm-yy").parse("06-12-80"));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		r.setVoorletters("JW");
+
+		session.save(r);
+
+		t.commit();
+		System.out.println("successfully saved");
+		factory.close();
+		session.close();
 	}
 }
